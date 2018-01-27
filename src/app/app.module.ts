7b765aa6b8b21fb32b67236349a3851f53a52fab
Kyle Silberbauer <kyle.silberbauer@gmail.com>
2018-01-26 19:10:57 -0500

    moved workout details to separate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { WorkoutsComponent } from './workouts/workouts.component';
+import { WorkoutDetailComponent } from './workout-detail/workout-detail.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     WorkoutsComponent,
+    WorkoutDetailComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/workout-detail/workout-detail.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+import { Workout } from '../workout';
+
+@Component({
+  selector: 'app-workout-detail',
+  templateUrl: './workout-detail.component.html',
+  styleUrls: ['./workout-detail.component.css']
+})
+export class WorkoutDetailComponent implements OnInit {
+
+  @Input() workout: Workout;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
